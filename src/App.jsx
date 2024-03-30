eca845e7ce11cc6f83e8068f7843e36cd3a4772e
Morymirco <morykoulibaly996@gmail.com>
2024-03-30 11:54:42 +0000

    be

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,42 @@ import axios from "axios";
 function App() {
   const [meteo, setmeteo] = useState("");
   const [icon, seticon] = useState("");
+  const [info, setinfo] = useState("");
+const [error,seterror]=useState(false)
+  const [url, seturl] = useState("https://api.openweathermap.org/data/2.5/weather?q=conakry&units=Metric&appid=345ace78bd38d73a66e10f79953b394b");
+  function search(){
 
+    seturl(`https://api.openweathermap.org/data/2.5/weather?q=${info}&units=Metric&appid=345ace78bd38d73a66e10f79953b394b`)
+    console.log(url)
+    console.log(error)
+  }
+  function handlechange(e){
+    setinfo(e.target.value)
+  }
   useEffect(() => {
     axios
       .get(
-        "https://api.openweathermap.org/data/2.5/weather?q=conakry&units=Metric&appid=345ace78bd38d73a66e10f79953b394b"
+        url
       )
       .then((response) => {
         console.log(response.data.weather[0].icon);
         seticon(response.data.weather[0].icon);
         setmeteo(response.data.main.temp);
+        seterror(false)
       })
       .catch((error) => {
+        seterror(true)
         console.log(error);
-      });
-  }, []);
+      }); 
+  }, [url]);
 
   return (
     <>
       <div className="box">
-        <h1>Conakry </h1>
-        <p style={{ fontSize: "32px" }}> Méteo : {meteo} °C</p>
+        <input type="search" onChange={handlechange} />
+        <button className="btn" onClick={search}>Réchercher </button>
+        <h1>{error ? <p style={{fontSize:"12px"}}>Aucune correspondance</p> :info}</h1>
+        <p style={{ fontSize: "32px" }}> Méteo : {error ? "-" :meteo} °C</p>
 
         <img
           src={
@@ -37,8 +52,7 @@ function App() {
           }
           alt="meteo icone"
         />
-
-        <button className="btn">Découvrir plus </button>
+      
       </div>
     </>
   );
